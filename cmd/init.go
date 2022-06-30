8d7c102ed93eb1ba89c62763bf9d57b4f8c00a9c
Roger Souza <rogersilvasouza@hotmail.com>
2022-06-29 22:29:18 -0300

    feat: 🎸 add creation of file exists and .migration ideia

--- cmd/init.go
+++ cmd/init.go
@@ -5,6 +5,7 @@ Copyright © 2022 ROGER SOUZA <rogersilvasouza@hotmail.com>
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -16,7 +17,13 @@ var initCmd = &cobra.Command{
 	Short: "Start pluto in a new project",
 	Long:  `Init create a new project with the following files:`,
 	Run: func(cmd *cobra.Command, args []string) {
-		createFile()
+		if FileExist("pluto.yaml") {
+			CreateFile("pluto.yaml")
+		}
+
+		if FileExist(".migration") {
+			CreateFile(".migration")
+		}
 	},
 }
 
@@ -24,12 +31,21 @@ func init() {
 	rootCmd.AddCommand(initCmd)
 }
 
-func createFile() {
-	file, err := os.Create("pluto.yaml")
-
+func CreateFile(fileName string) {
+	file, err := os.Create(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	fmt.Printf("Created file: %s\n", fileName)
 	defer file.Close()
+}
+
+func FileExist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	fmt.Println("err:", err)
+
+	x := os.IsNotExist(err)
+	fmt.Println("x:", x)
+
+	return x
 }
