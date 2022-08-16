599415515162bf42d98ea83e6098a6a7ad34e41a
Roger Souza <rogersilvasouza@hotmail.com>
2022-08-16 18:16:23 -0300

    feat: Add Tests validating storage

---
+++ tests/storage_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"os"
+	"pluto/internal/storage"
+
+	"testing"
+)
+
+func TestStorage(t *testing.T) {
+	t.Run("Verify file is created", func(t *testing.T) {
+		var fileName string = "test.txt"
+
+		os.Remove(fileName)
+
+		storage.CreateFile(fileName)
+
+		_, err := os.Stat(fileName)
+		var notExists bool = os.IsNotExist(err)
+		if notExists {
+			t.Errorf("File %s not created", fileName)
+		}
+
+		var error error = os.Remove(fileName)
+		if error != nil {
+			log.Fatal(error)
+		}
+	})
+
+	t.Run("Check for existence of the file inside the folder", func(t *testing.T) {
+		var filePath string = "tests"
+		var fileName string = "test.txt"
+
+		os.Remove(fileName)
+
+		file, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		if !storage.FileExist(filePath + "/" + fileName) {
+			t.Errorf("File %s dont exists", filePath+"/"+fileName)
+		}
+
+		var error error = os.Remove(fileName)
+		if error != nil {
+			log.Fatal(error)
+		}
+	})
+}
