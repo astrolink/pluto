90760cdabb3cf3d547a519d190fb194ffa836eb0
Roger Souza <rogersilvasouza@hotmail.com>
2022-11-14 13:34:12 -0300

    feat: On Root cmd add short and long description

--- cmd/root.go
+++ cmd/root.go
@@ -9,18 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go",
-	Short: "A brief description of your application",
-	Long:  `A longer description`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "",
+	Short: "Pluto added migrations to your project",
+	Long:  `Your project should have the configuration file and migrations folder, pluto was built to be able to have to help solve this problem`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -29,13 +23,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("toggle", "t", false, "Use pluto init to start your project")
 }
