77ada00f625f745da2e2e7510f9b380aad8aa03c
Roger Souza <rogersilvasouza@hotmail.com>
2022-11-14 10:24:02 -0300

    feat: Migrated run aplication to other class

--- cmd/run.go
+++ cmd/run.go
@@ -4,15 +4,9 @@ Copyright © 2022 ROGER SOUZA <rogersilvasouza@hotmail.com>
 package cmd
 
 import (
-	"io/fs"
-	"strings"
-
 	"github.com/spf13/cobra"
 
 	"github.com/astrolink/pluto/general/pluto"
-	"github.com/astrolink/pluto/internal/database/mysql"
-	"github.com/astrolink/pluto/internal/database/postgre"
-	"github.com/astrolink/pluto/internal/storage"
 )
 
 var runCmd = &cobra.Command{
@@ -20,31 +14,10 @@ var runCmd = &cobra.Command{
 	Short: "Run migrations",
 	Long:  `Long Description`,
 	Run: func(cmd *cobra.Command, args []string) {
-		executeRun()
-
 		pluto.RunMigrations()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
-}
-
-func executeRun() {
-	var files []fs.FileInfo = storage.ReadFiles()
-
-	for _, file := range files {
-		if !file.IsDir() && strings.Contains(file.Name(), ".xml") {
-			result := storage.ReadXml(file.Name())
-
-			switch result.Database {
-			case "postgre":
-				postgre.Execute(result, file.Name(), "run")
-			case "mysql":
-				mysql.Execute(result, file.Name(), "run")
-			default:
-				mysql.Execute(result, file.Name(), "run")
-			}
-		}
-	}
 }
