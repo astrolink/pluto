2d75970eb0fd88fbaa6ee15bb9a51ee6b422c171
Phelipe Galiotti <pgaliotti@esapiens.com.br>
2022-11-17 11:24:00 -0300

    feat(version): - adicionando o comando version no pluto

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+)
+
+var greenLite = lipgloss.NewStyle().Foreground(lipgloss.Color("#7CFC00"))
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Get the pluto project version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(greenLite.Render("Version: v0.1.10 \nPluto is installed correctly. Enjoy! 😎"))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
